Fixed sqrt(2) factor in erf() in significance calculation

watroo/wavelets.py
@@ -106,7 +106,7 @@ class Coefficients:
         if soft_threshold:
             if sigma != 0:
                 r = np.abs(self.data[scale] / (sigma * self.noise * sigma_e))
-                return special.erf(r / np.sqrt(2))
+                return special.erf(r)
                 # return r / (1 + r)
             else:
                 return 1